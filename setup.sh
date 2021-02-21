5f9fe90308ea048be585d8fe0463749a916aa3ce
DerDipper <stefan-dennig@web.de>
2021-02-21 20:58:01 +0100

    added python install

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+#Setup Wake On Lan
 ./setup_wol.sh
+#Install Ifconfig
 ./setup_networkTools.sh
+#give ssh access
 ./setup_ssh.sh
+#install git server
 ./setup_git.sh
+#install python3 and pip3
+./setup_python.sh
 
 
